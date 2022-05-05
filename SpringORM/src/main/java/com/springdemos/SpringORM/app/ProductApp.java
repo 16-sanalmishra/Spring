a628664858d73ef105b9806905bb6eb6d6548a84
Sanal Mishra <sanamishra@BLR.ALLEGISINDIA.COM>
2022-05-05 10:55:48 +0530

    Spring ORM CRUD

--- SpringORM/src/main/java/com/springdemos/SpringORM/app/ProductApp.java
+++ SpringORM/src/main/java/com/springdemos/SpringORM/app/ProductApp.java
@@ -1,10 +1,14 @@
 package com.springdemos.SpringORM.app;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.springdemos.SpringORM.product.dao.ProductDao;
 import com.springdemos.SpringORM.product.entity.Product;
+
+
 
 public class ProductApp 
 {
@@ -13,12 +17,44 @@ public class ProductApp
     	ApplicationContext context = new ClassPathXmlApplicationContext("com/springdemos/SpringORM/config/config.xml");
 		ProductDao productdao = (ProductDao) context.getBean("productDaoImpl");
 		
-		createProduct(productdao);
+//		createProduct(productdao);
+//		updateProduct(productdao);
+//		deleteProduct(productdao);
+//		findProduct(productdao, 2);
+		findAllProducts(productdao);
+	}
+
+	private static void findAllProducts(ProductDao productdao) {
+		List<Product> products = productdao.findAll();
+		System.out.println(products);
+	}
+
+	private static void findProduct(ProductDao productdao, int id) {
+		Product product = productdao.find(id);
+		System.out.println(product);
+	}
+
+	private static void deleteProduct(ProductDao productdao) {
+		Product product = new Product();
+		product.setId(1);
+		productdao.delete(product);
+		
+		System.out.println("Rows deleted ");
+	}
+
+	private static void updateProduct(ProductDao productdao) {
+		Product product = new Product();
+		product.setId(1);
+		product.setName("iWatch");
+		product.setDescription("best watch");
+		product.setPrice(18000);
+		productdao.update(product);
+		System.out.println("Rows Updated ");
 	}
 
 	private static void createProduct(ProductDao productdao) {
 		Product product = new Product();
-		product.setId(1);
+		product.setId(2);
 		product.setName("iWatch");
 		product.setDescription("best watch");
 		product.setPrice(21560);
--- SpringORM/src/main/java/com/springdemos/SpringORM/product/dao/ProductDao.java
+++ SpringORM/src/main/java/com/springdemos/SpringORM/product/dao/ProductDao.java
@@ -1,8 +1,14 @@
 package com.springdemos.SpringORM.product.dao;
 
+import java.util.List;
+
 import com.springdemos.SpringORM.product.entity.Product;
 
 public interface ProductDao {
 
 	int create(Product product);
+	void update(Product product);
+	void delete(Product product);
+	Product find(int id);
+	List<Product> findAll();
 }
